d944315ee6f2fcd24f4fd078741b8eb895a4089a
vladgibeskul <wgv96300@gmail.com>
2025-03-10 18:52:51 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = 14;
+            int[] myArray = Input(arraySize);
+            int evenCount = Value(myArray);
+            Console.WriteLine($"Количество четных элементов в массиве: {evenCount}");
+            Console.ReadKey();
+        }
+        public static int[] Input(int size)
+        {
+            int[] array = new int[size];
+            Console.WriteLine($"Введите {size} элементов массива:");
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Элемент [{i}]: ");
+                if (int.TryParse(Console.ReadLine(), out int element))
+                {
+                    array[i] = element;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода. Пожалуйста, введите целое число.");
+                    i--;
+                }
+            }
+            return array;
+        } 
+        
+        public static int Value(int[] array)
+        {
+            int count = 0;
+            foreach (int element in array)
+            {
+                if (element % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
